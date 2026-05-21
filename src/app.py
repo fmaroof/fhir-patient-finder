6859f45f85f3547365baac4c68868b2660323155
fmaroof <105441790+fmaroof@users.noreply.github.com>
2026-05-21 11:43:07 -0400

    Switch to localhost FHIR server for local WintEHR testing; add .env to .gitignore

--- src/app.py
+++ src/app.py
@@ -6,37 +6,70 @@ from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-FHIR_SERVER_BASE_URL="http://pwebmedcit.services.brown.edu:8081/fhir"
+# FHIR_SERVER_BASE_URL="http://pwebmedcit.services.brown.edu:8081/fhir"
+FHIR_SERVER_BASE_URL="http://localhost:8888/fhir"
 
 load_dotenv()
 
 username = os.getenv("FHIR_USERNAME")
 password = os.getenv("FHIR_PASSWORD")
 
-def request_medications(code_id, credentials):
 
-    #url = f"{FHIR_SERVER_BASE_URL}/MedicationRequest?patient={patient_id}"
-    url = f"{FHIR_SERVER_BASE_URL}/MedicationRequest?_has:Observation:code={code_id}"
-    req = requests.get(url, auth = credentials)
-    
-    medications = req.json().get("entry",[])
-    return medications
+def request_patient(patient_id, credentials):
 
-@app.route('/', methods=['GET', 'POST'])
+    req = requests.get(
+        FHIR_SERVER_BASE_URL + "/Patient/" + str(patient_id), auth=credentials
+    )
+
+    print(f"Requests status: {req.status_code}")
+
+    response = req.json()
+    print(response.keys())
+
+    return response
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    
+
     result = None
     credentials = (username, password)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         try:
-            number = int(request.form['number'])
-            result = request_medications(number, credentials=credentials)
+            patient_id = request.form["number"].strip()
+            result = request_patient(patient_id, credentials=credentials)
+    
         except ValueError:
-            result = 'Invalid input. Please enter a SNOMED code.'
+            result = "Invalid input. Please enter a number."
 
-    return render_template('index.html', medications=result)
+    return render_template("index.html", result=result)
+
+
+# def request_medications(code_id, credentials):
+
+#     #url = f"{FHIR_SERVER_BASE_URL}/MedicationRequest?patient={patient_id}"
+#     url = f"{FHIR_SERVER_BASE_URL}/MedicationRequest?_has:Observation:code={code_id}"
+#     req = requests.get(url, auth = credentials)
+
+#     medications = req.json().get("entry",[])
+#     return medications
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+
+#     result = None
+#     credentials = (username, password)
+
+#     if request.method == 'POST':
+#         try:
+#             number = int(request.form['number'])
+#             result = request_medications(number, credentials=credentials)
+#         except ValueError:
+#             result = 'Invalid input. Please enter a SNOMED code.'
+
+#     return render_template('index.html', medications=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    port = int(os.environ.get("FHIR_PORT", 5001))
+    app.run(debug=True, port=port)
